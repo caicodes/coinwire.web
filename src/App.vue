e1706b137994dd5a248772cf64e9c27ef4530453
caicodes <caicodes1@gmail.com>
2022-09-06 14:59:47 -0600

    cleanup for content addition

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,8 @@
 import Code from "./components/mockups/Code.vue"
 import NavBar from "./components/app/NavBar.vue"
 import Screen from "./components/Screen.vue"
-import { btnListFull } from "./constants/Styleguide"
 import Footer from "./components/app/Footer.vue"
 import Hero from "./components/screens/Hero.vue"
-const btnList = btnListFull
-const btnSizes = ["xs", "sm", "md", "lg", "wide", "block", "circle", "square"]
 </script>
 
 <template>
@@ -20,12 +17,7 @@ const btnSizes = ["xs", "sm", "md", "lg", "wide", "block", "circle", "square"]
       </div>
     </Screen>
     <Screen id="overview" class="bg-base-300">
-      <div class="box box-3 flex">
-        <div v-for="item in btnList" class="p-8 m-1 cursor-pointer"
-          :class="`bg-${item}/10 hover:bg-${item} text-${item}`" :key="item">
-          {{ item }}
-        </div>
-      </div>
+      overview...
     </Screen>
     <Screen id="portfolio" class="bg-secondary-focus/10">
       <div class="box box-4 flex space-x-2">
@@ -40,38 +32,14 @@ const btnSizes = ["xs", "sm", "md", "lg", "wide", "block", "circle", "square"]
         <button class="btn btn-outline btn-primary">Primary</button>
         <button class="btn btn-outline btn-secondary">Secondary</button>
         <button class="btn btn-outline btn-accent">Accent</button>
-        <div class="p-4 bg-black"><button class="btn glass">glass</button></div>
-        <div class="p-4 bg-black/80">
-          <button class="btn glass">glass</button>
-        </div>
-        <div class="p-4 bg-white"><button class="btn glass">glass</button></div>
-        <div class="p-4 bg-base-300">
-          <button class="btn glass">glass</button>
-        </div>
-        <div class="p-4 bg-primary">
-          <button class="btn glass">glass</button>
-        </div>
+
       </div>
     </Screen>
     <Screen id="about" class="bg-primary/10">
-      <div v-for="size in btnSizes" :key="size" class="space-x-2 space-y-2">
-        <button v-for="item in btnList" :key="item" class="btn" :class="`btn-${item} btn-${size}`">
-          {{ item }} {{ size }}
-        </button>
-        <button class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">
-          Responsive
-        </button>
-      </div>
+      about
     </Screen>
     <Screen id="faq" class="bg-primary/5">
-      <div v-for="size in btnSizes" :key="size" class="space-x-2 space-y-2">
-        <button v-for="item in btnList" :key="item" class="btn" :class="`btn-${item} btn-${size}`">
-          {{ item }} {{ size }}
-        </button>
-        <button class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">
-          Responsive
-        </button>
-      </div>
+      faq...
     </Screen>
     <Footer />
   </div>
