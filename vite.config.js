0fe30942aa7d6ea8c478480b5020e0c673cfa9db
caicodes <caicodes1@gmail.com>
2022-09-06 14:51:24 -0600

    removing old plugin defs

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite"
-import vue from "@vitejs/plugin-vue"
-import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vanillaExtractPlugin()],
+  plugins: [vue()],
   server: {
     host: "0.0.0.0",
   },
