70a84b0634ab059102771a08b93e9759a190d97f
caicodes <caicodes1@gmail.com>
2022-09-11 15:04:15 -0600

    footer updated

--- src/components/app/Footer.vue
+++ src/components/app/Footer.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="h-96">
-    Complete cryptocurrency market coverage with live coin prices, charts and
-    crypto market cap featuring 18967 coins on 506 exchanges.
+  <div class="h-96 flex flex-col place-items-center">
+
+    <div class="text-2xl p-20 mx-auto">Complete cryptocurrency market coverage with live coin prices, charts
+      and crypto market cap featuring 18967 coins on 506 exchanges.
+    </div>
+    <button class="mx-auto btn btn-primary">Register Now</button>
+
   </div>
 </template>
